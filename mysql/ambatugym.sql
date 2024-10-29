c2100c9ea1274a856ed48b0923b6d0062fe12d29
edwin <114722452+clunckyboy@users.noreply.github.com>
2024-10-29 11:49:50 +0700

    Create ambatugym.sql

---
+++ mysql/ambatugym.sql
@@ -0,0 +1,302 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 29, 2024 at 05:39 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ambatugym`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `badge`
+--
+
+CREATE TABLE `badge` (
+  `badge_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `criteria` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `comment_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `community_post`
+--
+
+CREATE TABLE `community_post` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `post_date` timestamp NULL DEFAULT NULL,
+  `likes_count` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises`
+--
+
+CREATE TABLE `exercises` (
+  `exercise_id` int(11) NOT NULL,
+  `exercise_name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `goal` enum('lose_weight','build_muscle','maintain') NOT NULL,
+  `exercise_type` enum('cardio','strength','flexibility','balance') NOT NULL,
+  `exercises_duration` int(50) NOT NULL,
+  `video_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `users_id` int(11) NOT NULL,
+  `users_fullname` varchar(100) NOT NULL,
+  `users_email` int(11) NOT NULL,
+  `users_username` varchar(50) NOT NULL,
+  `users_password` varchar(255) NOT NULL,
+  `users_gender` enum('Laki-laki','Perempuan') NOT NULL,
+  `users_age` int(11) NOT NULL,
+  `users_birthdate` date DEFAULT current_timestamp(),
+  `users_weight` decimal(65,0) NOT NULL,
+  `users_height` decimal(65,0) NOT NULL,
+  `goal` enum('lose_weight','build_muscle','maintain') NOT NULL,
+  `users_photo` int(11) NOT NULL,
+  `users_badge` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`users_badge`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`users_id`, `users_fullname`, `users_email`, `users_username`, `users_password`, `users_gender`, `users_age`, `users_birthdate`, `users_weight`, `users_height`, `goal`, `users_photo`, `users_badge`) VALUES
+(1, '', 0, 'anna', '123', '', 0, '2024-10-22', 0, 0, '', 0, '0'),
+(2, '', 0, 'abv', '123', '', 0, '2024-10-22', 0, 0, '', 0, '0'),
+(3, '', 0, 'cth', '123', '', 0, '2024-10-22', 0, 0, '', 0, '0'),
+(6, '', 0, 'boku', '321', '', 0, '2024-10-22', 0, 0, '', 0, '0'),
+(8, '', 0, 'muani', '123', '', 0, '2024-10-22', 0, 0, '', 0, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_badge`
+--
+
+CREATE TABLE `user_badge` (
+  `user_badge_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `badge_id` int(11) NOT NULL,
+  `date_earned` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_stat`
+--
+
+CREATE TABLE `user_stat` (
+  `user_id` int(11) NOT NULL,
+  `total_workout_complete` int(11) NOT NULL,
+  `total_calories_burned` decimal(10,0) NOT NULL,
+  `streak_days` int(11) NOT NULL,
+  `last_active` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_workout_progress`
+--
+
+CREATE TABLE `user_workout_progress` (
+  `progress_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `exercise_id` int(11) NOT NULL,
+  `progress_date` date NOT NULL DEFAULT current_timestamp(),
+  `status` enum('completed','skipped') NOT NULL,
+  `calories_burned` decimal(50,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `badge`
+--
+ALTER TABLE `badge`
+  ADD PRIMARY KEY (`badge_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Indexes for table `community_post`
+--
+ALTER TABLE `community_post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `exercises`
+--
+ALTER TABLE `exercises`
+  ADD PRIMARY KEY (`exercise_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`),
+  ADD UNIQUE KEY `users_username` (`users_username`);
+
+--
+-- Indexes for table `user_badge`
+--
+ALTER TABLE `user_badge`
+  ADD PRIMARY KEY (`user_badge_id`),
+  ADD KEY `users_id` (`users_id`),
+  ADD KEY `badge_id` (`badge_id`);
+
+--
+-- Indexes for table `user_stat`
+--
+ALTER TABLE `user_stat`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user_workout_progress`
+--
+ALTER TABLE `user_workout_progress`
+  ADD PRIMARY KEY (`progress_id`),
+  ADD KEY `p` (`exercise_id`),
+  ADD KEY `user id relation` (`users_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `badge`
+--
+ALTER TABLE `badge`
+  MODIFY `badge_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `community_post`
+--
+ALTER TABLE `community_post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exercises`
+--
+ALTER TABLE `exercises`
+  MODIFY `exercise_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_badge`
+--
+ALTER TABLE `user_badge`
+  MODIFY `user_badge_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_workout_progress`
+--
+ALTER TABLE `user_workout_progress`
+  MODIFY `progress_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `community_post` (`post_id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`);
+
+--
+-- Constraints for table `community_post`
+--
+ALTER TABLE `community_post`
+  ADD CONSTRAINT `community_post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`users_id`);
+
+--
+-- Constraints for table `user_badge`
+--
+ALTER TABLE `user_badge`
+  ADD CONSTRAINT `user_badge_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`),
+  ADD CONSTRAINT `user_badge_ibfk_2` FOREIGN KEY (`badge_id`) REFERENCES `badge` (`badge_id`);
+
+--
+-- Constraints for table `user_stat`
+--
+ALTER TABLE `user_stat`
+  ADD CONSTRAINT `user_stat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`users_id`);
+
+--
+-- Constraints for table `user_workout_progress`
+--
+ALTER TABLE `user_workout_progress`
+  ADD CONSTRAINT `p` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`exercise_id`),
+  ADD CONSTRAINT `user id relation` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
