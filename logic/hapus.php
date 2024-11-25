2c744340f0084db25357208a6d988626446231ba
edwin <114722452+clunckyboy@users.noreply.github.com>
2024-11-25 19:11:41 +0700

    perbaiki bug

--- logic/hapus.php
+++ logic/hapus.php
@@ -1,10 +1,5 @@
 <?php
     session_start();
-
-    if ( !isset($_SESSION["is_login"]) ){
-        header("location: login.php");
-        exit;
-    }
     
     require '../database/config.php';
     $id = $_GET["id"];
